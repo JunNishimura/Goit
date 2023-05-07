4e4d3c9ee1e20a9be0520a9380d1b1280d68d3d5
Jun Nishimura <n.junjun0303@gmail.com>
2023-05-08 01:35:21 +0900

    hashObjectCmdの実装 #12

--- cmd/hashObject.go
+++ cmd/hashObject.go
@@ -4,8 +4,10 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"errors"
 	"fmt"
 
+	"github.com/JunNishimura/Goit/object"
 	"github.com/spf13/cobra"
 )
 
@@ -14,8 +16,21 @@ var hashObjectCmd = &cobra.Command{
 	Use:   "hash-object",
 	Short: "calculate the hash of the file",
 	Long:  "calculate the hash of the file",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("hashObject called")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if !IsGoitInitialized() {
+			return errors.New("fatal: not a goit repository: .goit")
+		}
+
+		for _, arg := range args {
+			obj, err := object.NewBlobObject(arg)
+			if err != nil {
+				return err
+			}
+
+			fmt.Println(obj.Hash.String())
+		}
+
+		return nil
 	},
 }
 
