dce13cc807ba1061f8a79dd3ddedf6f5ed32c682
Jun Nishimura <n.junjun0303@gmail.com>
2023-05-13 22:06:25 +0900

    add makeTreeObject #20

--- cmd/writeTree.go
+++ cmd/writeTree.go
@@ -6,10 +6,76 @@ package cmd
 import (
 	"errors"
 	"fmt"
+	"strings"
 
+	"github.com/JunNishimura/Goit/index"
 	"github.com/JunNishimura/Goit/object"
 	"github.com/spf13/cobra"
 )
+
+func makeTreeObject(entries []*index.Entry) *object.Object {
+	var dirName string
+	var data []byte
+	var entryBuf []*index.Entry
+	i := 0
+	for {
+		if i >= len(entries) {
+			// if the last entry is in the directory
+			if dirName != "" {
+				treeObject := makeTreeObject(entryBuf)
+				data = append(data, []byte(dirName)...)
+				data = append(data, 0x00)
+				data = append(data, treeObject.Hash...)
+			}
+			break
+		}
+
+		entry := entries[i]
+		slashSplit := strings.SplitN(string(entry.Path), "/", 2)
+		if len(slashSplit) == 1 {
+			if dirName != "" {
+				// make tree object from entryBuf
+				treeObject := makeTreeObject(entryBuf)
+				data = append(data, []byte(dirName)...)
+				data = append(data, 0x00)
+				data = append(data, treeObject.Hash...)
+				// clear dirName and entryBuf
+				dirName = ""
+				entryBuf = make([]*index.Entry, 0)
+			} else {
+				data = append(data, entry.Path...)
+				data = append(data, 0x00)
+				data = append(data, entry.Hash...)
+				i++
+			}
+		} else {
+			if dirName == "" {
+				dirName = slashSplit[0]
+				newEntry := index.NewEntry(entry.Hash, []byte(slashSplit[1]))
+				entryBuf = append(entryBuf, newEntry)
+				i++
+			} else if dirName != "" && dirName == slashSplit[0] {
+				// same dir with prev entry
+				newEntry := index.NewEntry(entry.Hash, []byte(slashSplit[1]))
+				entryBuf = append(entryBuf, newEntry)
+				i++
+			} else if dirName != "" && dirName != slashSplit[0] {
+				treeObject := makeTreeObject(entryBuf)
+				data = append(data, []byte(dirName)...)
+				data = append(data, 0x00)
+				data = append(data, treeObject.Hash...)
+				// clear dirName and entryBuf
+				dirName = ""
+				entryBuf = make([]*index.Entry, 0)
+			}
+		}
+	}
+
+	// make tree object
+	treeObject := object.NewObject(object.TreeObject, data)
+
+	return treeObject
+}
 
 // writeTreeCmd represents the writeTree command
 var writeTreeCmd = &cobra.Command{
@@ -21,23 +87,16 @@ var writeTreeCmd = &cobra.Command{
 			return errors.New("fatal: not a goit repository: .goit")
 		}
 
-		// make data from index
-		var data []byte
-		for _, entry := range indexClient.Entries {
-			data = append(data, entry.Path...)
-			data = append(data, 0x00)
-			data = append(data, entry.Hash...)
-		}
+		// make treeObject from index
+		rootTreeObject := makeTreeObject(indexClient.Entries)
 
-		// make tree object
-		treeObject := object.NewObject(object.TreeObject, data)
-
-		if err := treeObject.Write(); err != nil {
+		// write root tree object
+		if err := rootTreeObject.Write(); err != nil {
 			return fmt.Errorf("fail to write tree object: %v", err)
 		}
 
 		// print out tree object hash
-		fmt.Printf("%s\n", treeObject.Hash)
+		fmt.Printf("%s\n", rootTreeObject.Hash)
 
 		return nil
 	},
