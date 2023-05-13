661170834321a757512328f6156d6e9c9d36e1f7
Jun Nishimura <n.junjun0303@gmail.com>
2023-05-13 16:57:13 +0900

    delete deleteFromIndex function from add.go

--- cmd/add.go
+++ cmd/add.go
@@ -4,55 +4,19 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"bufio"
 	"errors"
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/JunNishimura/Goit/object"
 	"github.com/spf13/cobra"
 )
 
-const (
-	INDEX_PATH = ".goit/index"
-)
-
-func deleteFromIndex() error {
-	f, err := os.Open(INDEX_PATH)
-	if err != nil {
-		return fmt.Errorf("fail to open %s: %v", INDEX_PATH, err)
-	}
-
-	// remove files to be deleted from index
-	var lines []string
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		blobInfo := strings.Split(scanner.Text(), " ")
-		if _, err := os.Stat(blobInfo[1]); !os.IsNotExist(err) {
-			lines = append(lines, strings.Join(blobInfo, " "))
-		}
-	}
-	f.Close()
-
-	// rewrite index
-	f, err = os.OpenFile(INDEX_PATH, os.O_RDWR|os.O_TRUNC, 0666)
-	if err != nil {
-		return fmt.Errorf("fail to open %s: %v", INDEX_PATH, err)
-	}
-	for _, line := range lines {
-		fmt.Fprintln(f, line)
-	}
-	defer f.Close()
-
-	return nil
-}
-
 // addCmd represents the add command
 var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "register changes to index",
-	Long:  `This is a command to register changes to index.`,
+	Long:  "This is a command to register changes to index.",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if !IsGoitInitialized() {
 			return errors.New("fatal: not a goit repository: .goit")
