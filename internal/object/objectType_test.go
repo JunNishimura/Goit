031617067b2ed5aa77c95a5c5a88ac9374761ae1
Jun Nishimura <n.junjun0303@gmail.com>
2023-05-22 01:36:50 +0900

    update objectType_test (#44)

--- internal/object/objectType_test.go
+++ internal/object/objectType_test.go
@@ -1,55 +1,68 @@
 package object
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 func TestNewType(t *testing.T) {
 	type args struct {
 		typeString string
 	}
 	tests := []struct {
-		name string
-		args args
-		want Type
+		name    string
+		args    args
+		want    Type
+		wantErr error
 	}{
 		{
 			name: "blob",
 			args: args{
 				typeString: "blob",
 			},
-			want: BlobObject,
+			want:    BlobObject,
+			wantErr: nil,
 		},
 		{
 			name: "tree",
 			args: args{
 				typeString: "tree",
 			},
-			want: TreeObject,
+			want:    TreeObject,
+			wantErr: nil,
 		},
 		{
 			name: "commit",
 			args: args{
 				typeString: "commit",
 			},
-			want: CommitObject,
+			want:    CommitObject,
+			wantErr: nil,
 		},
 		{
 			name: "tag",
 			args: args{
 				typeString: "tag",
 			},
-			want: TagObject,
+			want:    TagObject,
+			wantErr: nil,
 		},
 		{
 			name: "undefined",
 			args: args{
 				typeString: "xxx",
 			},
-			want: UndefinedObject,
+			want:    UndefinedObject,
+			wantErr: ErrInvalidObject,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got, _ := NewType(tt.args.typeString); got != tt.want {
+			got, err := NewType(tt.args.typeString)
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("got = %v, want = %v", err, tt.wantErr)
+			}
+			if got != tt.want {
 				t.Errorf("got = %s, want = %s", got, tt.want)
 			}
 		})
