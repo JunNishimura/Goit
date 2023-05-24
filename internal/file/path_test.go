c7fff00f08a2559d1306fda302057632a951a1e3
Jun Nishimura <n.junjun0303@gmail.com>
2023-05-25 00:05:40 +0900

    update path_test (#44)
    remove GoitInit

--- internal/file/path_test.go
+++ internal/file/path_test.go
@@ -5,53 +5,94 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
-
-	testUtil "github.com/JunNishimura/Goit/internal/internal/util"
 )
 
 func TestFindGoitRoot(t *testing.T) {
 	type args struct {
 		path string
 	}
-	type test struct {
+	tests := []struct {
 		name    string
 		args    args
 		want    string
 		wantErr error
-	}
-	tests := []*test{
-		func() *test {
-			rootPath, _ := filepath.Abs("../../")
-			testdataPath := filepath.Join(rootPath, "testdata")
-			goitPath := filepath.Join(testdataPath, ".goit")
-
-			return &test{
-				name: "success",
-				args: args{
-					path: testdataPath,
-				},
-				want:    goitPath,
-				wantErr: nil,
-			}
-		}(),
+	}{
+		{
+			name: "success",
+			args: args{
+				path: ".",
+			},
+			want:    ".goit",
+			wantErr: nil,
+		},
+		{
+			name: "fail",
+			args: args{
+				path: "../",
+			},
+			want:    "",
+			wantErr: ErrGoitRootNotFound,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			goitPath := filepath.Join(tt.args.path, ".goit")
-			err := testUtil.GoitInit()
-			if err != nil {
-				t.Logf("fail to initialize goit")
+			tmpDir := t.TempDir()
+			// .goit initialization
+			goitDir := filepath.Join(tmpDir, ".goit")
+			if err := os.Mkdir(goitDir, os.ModePerm); err != nil {
+				t.Logf("%v: %s", err, goitDir)
 			}
-			defer func() {
-				os.RemoveAll(goitPath)
-			}()
+			// make .goit/config file
+			configFile := filepath.Join(goitDir, "config")
+			f, err := os.Create(configFile)
+			if err != nil {
+				t.Logf("%v: %s", err, configFile)
+			}
+			f.Close()
+			// make .goit/HEAD file and write main branch
+			headFile := filepath.Join(goitDir, "HEAD")
+			f, err = os.Create(headFile)
+			if err != nil {
+				t.Logf("%v: %s", err, headFile)
+			}
+			// set 'main' as default branch
+			if _, err := f.WriteString("ref: refs/heads/main"); err != nil {
+				t.Logf("%v: %s", err, headFile)
+			}
+			f.Close()
+			// make .goit/objects directory
+			objectsDir := filepath.Join(goitDir, "objects")
+			if err := os.Mkdir(objectsDir, os.ModePerm); err != nil {
+				t.Logf("%v: %s", err, objectsDir)
+			}
+			// make .goit/refs directory
+			refsDir := filepath.Join(goitDir, "refs")
+			if err := os.Mkdir(refsDir, os.ModePerm); err != nil {
+				t.Logf("%v: %s", err, refsDir)
+			}
+			// make .goit/refs/heads directory
+			headsDir := filepath.Join(refsDir, "heads")
+			if err := os.Mkdir(headsDir, os.ModePerm); err != nil {
+				t.Logf("%v: %s", err, headsDir)
+			}
+			// make .goit/refs/tags directory
+			tagsDir := filepath.Join(refsDir, "tags")
+			if err := os.Mkdir(tagsDir, os.ModePerm); err != nil {
+				t.Logf("%v: %s", err, tagsDir)
+			}
 
-			goitRootPath, err := FindGoitRoot(tt.args.path)
+			rootPath := filepath.Join(tmpDir, tt.args.path)
+			goitRootPath, err := FindGoitRoot(rootPath)
 			if !errors.Is(err, tt.wantErr) {
 				t.Errorf("got = %v, want = %v", err, tt.wantErr)
 			}
-			if goitRootPath != tt.want {
-				t.Errorf("got = %v, want = %v", goitRootPath, tt.want)
+
+			var wantPath string
+			if tt.want != "" {
+				wantPath = filepath.Join(tmpDir, tt.want)
+			}
+			if goitRootPath != wantPath {
+				t.Errorf("got = %v, want = %v", goitRootPath, wantPath)
 			}
 		})
 	}
