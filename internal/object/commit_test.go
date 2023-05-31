a3eea3f6069eea7180fe0c55f72085d074f6fa25
Jun Nishimura <n.junjun0303@gmail.com>
2023-06-01 00:25:27 +0900

    add TestNewCommit (#44)

--- internal/object/commit_test.go
+++ internal/object/commit_test.go
@@ -1,6 +1,8 @@
 package object
 
 import (
+	"errors"
+	"reflect"
 	"testing"
 	"time"
 )
@@ -36,6 +38,43 @@ func TestNewSign(t *testing.T) {
 			}
 			if got.Email != tt.want.Email {
 				t.Errorf("got = %s, want = %s", got.Email, tt.want.Email)
+			}
+		})
+	}
+}
+
+func TestNewCommit(t *testing.T) {
+	type args struct {
+		obj *Object
+	}
+	type test struct {
+		name    string
+		args    args
+		want    *Commit
+		wantErr error
+	}
+	tests := []*test{
+		func() *test {
+			obj, _ := NewObject(BlobObject, []byte("blob 12\x00Hello, World"))
+
+			return &test{
+				name: "fail: blob object",
+				args: args{
+					obj: obj,
+				},
+				want:    nil,
+				wantErr: ErrNotCommitObject,
+			}
+		}(),
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewCommit(tt.args.obj)
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("got = %v, want = %v", err, tt.wantErr)
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("got = %v, want = %v", got, tt.want)
 			}
 		})
 	}
