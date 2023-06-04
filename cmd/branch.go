5f4ea288d89b17a146bd0d4b81770942e88cb25d
Jun Nishimura <n.junjun0303@gmail.com>
2023-06-04 23:51:38 +0900

    add list and rename (#98)

--- cmd/branch.go
+++ cmd/branch.go
@@ -5,7 +5,10 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
+	"github.com/JunNishimura/Goit/internal/store"
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
@@ -14,6 +17,46 @@ var (
 	renameOption string = ""
 	deleteOption string = ""
 )
+
+func list(client *store.Client) {
+	for _, branch := range client.Refs.Heads {
+		if branch.Name == client.Head.Reference {
+			color.Green("* %s", branch.Name)
+		} else {
+			fmt.Println(branch.Name)
+		}
+	}
+}
+
+func rename(client *store.Client, newName string) error {
+	// check if new name is not used for other branches
+	for _, branch := range client.Refs.Heads {
+		if branch.Name == newName {
+			return fmt.Errorf("fatal: branch named '%s' already exists", newName)
+		}
+	}
+
+	// rename current branch
+	branch, err := client.Refs.GetBranch(client.Head.Reference)
+	if err != nil {
+		return err
+	}
+	branch.Name = newName
+
+	// rename file
+	oldPath := filepath.Join(client.RootGoitPath, "refs", "heads", client.Head.Reference)
+	newPath := filepath.Join(client.RootGoitPath, "refs", "heads", newName)
+	if err := os.Rename(oldPath, newPath); err != nil {
+		return fmt.Errorf("fail to rename: %w", err)
+	}
+
+	// rename HEAD
+	if err := client.Head.Update(client.RootGoitPath, newName); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 // branchCmd represents the branch command
 var branchCmd = &cobra.Command{
@@ -40,12 +83,13 @@ var branchCmd = &cobra.Command{
 
 		// list branches
 		if isList {
-			for _, branch := range client.Refs.Heads {
-				if branch.Name == client.Head.Reference {
-					color.Green("* %s", branch.Name)
-				} else {
-					fmt.Println(branch.Name)
-				}
+			list(client)
+		}
+
+		// rename current branch
+		if renameOption != "" {
+			if err := rename(client, renameOption); err != nil {
+				return err
 			}
 		}
 
