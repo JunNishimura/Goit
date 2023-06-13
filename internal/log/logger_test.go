cba1f6b8cbb7e87fc233aae4a5437c8e4178722b
Jun Nishimura <n.junjun0303@gmail.com>
2023-06-14 01:08:54 +0900

    add TestWriteHEAD (#108)

--- internal/log/logger_test.go
+++ internal/log/logger_test.go
@@ -3,6 +3,8 @@ package log
 import (
 	"encoding/hex"
 	"fmt"
+	"os"
+	"path/filepath"
 	"reflect"
 	"testing"
 	"time"
@@ -125,6 +127,82 @@ func TestNewRecord(t *testing.T) {
 			got := NewRecord(tt.args.recType, tt.args.from, tt.args.to, tt.args.name, tt.args.email, tt.args.t, tt.args.message)
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("got = %v, want = %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestWriteHEAD(t *testing.T) {
+	type args struct {
+		rec *record
+	}
+	type test struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}
+	tests := []*test{
+		func() *test {
+			hash, _ := hex.DecodeString("87f3c49bccf2597484ece08746d3ee5defaba335")
+			now := time.Now()
+			rec := NewRecord(CommitRecord, hash, hash, "Test Taro", "test@example.com", now, "test")
+
+			return &test{
+				name: "success: commit record",
+				args: args{
+					rec: rec,
+				},
+				want:    fmt.Sprintf("%s %s %s <%s> %s %s\t%s: %s\n", rec.from, rec.to, rec.name, rec.email, rec.unixtime, rec.timeDiff, rec.recType, rec.message),
+				wantErr: false,
+			}
+		}(),
+		func() *test {
+			hash, _ := hex.DecodeString("87f3c49bccf2597484ece08746d3ee5defaba335")
+			now := time.Now()
+			rec := NewRecord(BranchRecord, hash, hash, "Test Taro", "test@example.com", now, "test")
+
+			return &test{
+				name: "success: branch record",
+				args: args{
+					rec: rec,
+				},
+				want:    fmt.Sprintf("%s %s %s <%s> %s %s\t%s: %s\n", rec.from, rec.to, rec.name, rec.email, rec.unixtime, rec.timeDiff, rec.recType, rec.message),
+				wantErr: false,
+			}
+		}(),
+		func() *test {
+			hash, _ := hex.DecodeString("87f3c49bccf2597484ece08746d3ee5defaba335")
+			now := time.Now()
+			rec := NewRecord(CheckoutRecord, hash, hash, "Test Taro", "test@example.com", now, "test")
+
+			return &test{
+				name: "success: checkout record",
+				args: args{
+					rec: rec,
+				},
+				want:    fmt.Sprintf("%s %s %s <%s> %s %s\t%s: %s\n", rec.from, rec.to, rec.name, rec.email, rec.unixtime, rec.timeDiff, rec.recType, rec.message),
+				wantErr: false,
+			}
+		}(),
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tmpDir := t.TempDir()
+
+			gLogger := NewGoitLogger(tmpDir)
+			if err := gLogger.WriteHEAD(tt.args.rec); (err != nil) != tt.wantErr {
+				t.Errorf("got = %v, want = %v", err, tt.wantErr)
+			}
+
+			headPath := filepath.Join(tmpDir, "logs", "HEAD")
+			b, err := os.ReadFile(headPath)
+			if err != nil {
+				t.Log(err)
+			}
+			got := string(b)
+			if got != tt.want {
+				t.Errorf("got = %s, want = %s", got, tt.want)
 			}
 		})
 	}
