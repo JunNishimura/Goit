08618e8dbc36c9f89d94833565a46706f2e80d50
Jun Nishimura <n.junjun0303@gmail.com>
2023-06-17 14:50:18 +0900

    add Compare method (#122)

--- internal/sha/sha_test.go
+++ internal/sha/sha_test.go
@@ -1,9 +1,65 @@
 package sha
 
 import (
+	"encoding/hex"
 	"errors"
+	"strings"
 	"testing"
 )
+
+func TestCompare(t *testing.T) {
+	type args struct {
+		sha SHA1
+	}
+	type fields struct {
+		sha SHA1
+	}
+	type test struct {
+		name   string
+		args   args
+		fields fields
+		want   bool
+	}
+	tests := []*test{
+		func() *test {
+			s, _ := hex.DecodeString("87f3c49bccf2597484ece08746d3ee5defaba335")
+
+			return &test{
+				name: "success: true",
+				args: args{
+					sha: s,
+				},
+				fields: fields{
+					sha: s,
+				},
+				want: true,
+			}
+		}(),
+		func() *test {
+			s, _ := hex.DecodeString("87f3c49bccf2597484ece08746d3ee5defaba335")
+			s2, _ := hex.DecodeString(strings.Repeat("0", 40))
+
+			return &test{
+				name: "success: false",
+				args: args{
+					sha: s,
+				},
+				fields: fields{
+					sha: s2,
+				},
+				want: false,
+			}
+		}(),
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.fields.sha.Compare(tt.args.sha)
+			if got != tt.want {
+				t.Errorf("got = %v, want = %v", got, tt.want)
+			}
+		})
+	}
+}
 
 func TestReadHash(t *testing.T) {
 	type args struct {
