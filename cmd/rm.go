46b1c28e218e9727823064109fa01bd36bdc9bf7
Jun Nishimura <n.junjun0303@gmail.com>
2023-06-21 08:46:43 +0900

    add args validation (#134)

--- cmd/rm.go
+++ cmd/rm.go
@@ -1,40 +1,45 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
-	"fmt"
+	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
+)
+
+var (
+	rFlag bool
 )
 
 // rmCmd represents the rm command
 var rmCmd = &cobra.Command{
 	Use:   "rm",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "remove file from the working tree and the index",
+	Long:  "remove file from the working tree and the index",
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		if client.RootGoitPath == "" {
+			return ErrGoitNotInitialized
+		}
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// args validation
+		for _, arg := range args {
+			// check if the arg is registered in the Index
+			cleanedArg := filepath.Clean(arg)
+			cleanedArg = strings.ReplaceAll(cleanedArg, `\`, "/")
+			client.Idx.GetEntry([]byte())
+		}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("rm called")
+		return nil
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(rmCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// rmCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// rmCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rmCmd.Flags().BoolVarP(&rFlag, "rec", "r", false, "allow recursive removal")
 }
